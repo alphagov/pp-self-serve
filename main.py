4bc8b129b3efc5fa4525589d64cc8f4d83545943
Henry Hadlow <henry@manofscience.co.uk>
2014-12-23 11:03:45 +0000

    Add Flask-Assets and pyScss
    to compile govuk_frontend_toolkit

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
-from flask import Flask
+
+import config
+
+from flask import Flask, render_template
+
+from flask.ext.assets import Environment, Bundle
+
+
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('css/main.scss', filters='pyscss', output='css/main.css')
+assets.register('scss_all', scss)
 
-if __name__ == "__main__":
+@app.route('/favicon.ico')
+def favicon():
+    return ''
+
+
+@app.route('/static/<path:path>', methods=['GET'])
+def static_files():
+    return send_from_directory(config.STATIC_PATH, path)
+
+# @app.route('/<path:path>', methods=['GET'])
+@app.route('/', methods=['GET'])
+# def render(path):
+def render():
+    return render_template('pp-self-serve.html',
+        title='Hello, world!',
+        content='Hello, world!',
+        govuk_template_path='/static/govuk_template/')
+
+if __name__ == '__main__':
     app.run()
